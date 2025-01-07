b366bf0f1894531e9e8667f0720d6ca57303c33e
bnElmookhtar <karimmotar16@gmail.com>
2025-01-07 01:07:30 -0800

    remove comments form app.py

--- app.py
+++ app.py
@@ -4,21 +4,16 @@ import numpy as np
 
 app = Flask(__name__)
 
-# تحميل النموذج المحفوظ
-model = joblib.load('manual_model.pkl')
+model = joblib.load('models/manual_model.pkl')
 
-# الصفحة الرئيسية لعرض نموذج الـ HTML
 @app.route('/')
 def home():
-    return render_template('predict.html')  # استخدام ملف predict.html من مجلد templates
+    return render_template('predict.html') 
 
-# دالة لاستقبال المدخلات والتنبؤ
 @app.route('/predict', methods=['POST'])
 def predict():
-    # استلام المدخلات من الـ HTML
     data = request.get_json()
 
-    # استخراج المدخلات (حولها إلى نوع بيانات مناسب)
     cp = int(data['cp'])
     slope = int(data['slope'])
     thalach = int(data['thalach'])
@@ -27,15 +22,11 @@ def predict():
     age = int(data['age'])
     exang = int(data['exang'])
 
-    # تحويل المدخلات إلى مصفوفة يمكن استخدامها من قبل النموذج
     input_features = np.array([[cp, slope, thalach, restecg, ca, age, exang]])
 
-    # إجراء التنبؤ باستخدام النموذج
     prediction = model.predict(input_features)
 
-    # إرجاع التنبؤ كنتيجة (1 أو 0)
     return jsonify({'prediction': str(prediction[0])})
 
-# تشغيل السيرفر
 if __name__ == '__main__':
     app.run(debug=True)
